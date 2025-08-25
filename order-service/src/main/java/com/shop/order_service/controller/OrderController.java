6ba5c224df4db3b652d30f563a36630839d6b5a2
Prazwal747 <prajwalshivapurmath@gmail.com>
2025-08-25 17:57:31 +0530

    added Logging configuration

--- order-service/src/main/java/com/shop/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/shop/order_service/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.shop.order_service.controller;
 
 
 import com.shop.order_service.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +17,16 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @PostMapping("/bookOrder")
     public TransactionResponse bookOrder(@RequestBody TransactionRequest transactionRequest){
 
+        logger.error("ERROR LOG");
+        logger.warn("Warning");
+        logger.info("Method invoked");
+        logger.debug("Debug started");
+        logger.trace("Trace log");
         return orderService.saveOrder(transactionRequest);
     }
 
